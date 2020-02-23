112f944ec91aa7294e0243a8784232619f6cd79a
hyeran1945@gmail.com <wlszh!23>
2020-02-23 23:06:19 +0900

    api 테스트

--- src/test/java/com/berna/webclient/WebClientCallTest.java
+++ src/test/java/com/berna/webclient/WebClientCallTest.java
@@ -1,17 +1,25 @@
 package com.berna.webclient;
 
+import com.berna.cache.domain.ApiResult;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@WebFluxTest
 public class WebClientCallTest {
 
-
     @Test
-    private void api_call_성공(){
+    public void api_call_성공(){
+        String url = "http://openapi.seoul.go.kr:8088/496270515468796534334167537975/json/GetParkInfo/1/1";
 
+         WebTestClient.bindToServer().baseUrl(url).build().get().uri(url).accept(MediaType.APPLICATION_JSON_UTF8)
+                 .exchange()
+                 .expectStatus().isOk()
+                 .expectBody(ApiResult.class);
     }
 
-    @Test
-    private void api_call_실패(){
-
-    }
 }
